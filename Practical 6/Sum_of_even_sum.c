34e49f749542f007bbd5558213b116b4d32f204d
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:22:40 +0530

    Implement sum of even numbers in Sum_of_even_sum.c
    Calculate the sum of even numbers from 1 to n.

--- Practical 6/Sum_of_even_sum.c
+++ Practical 6/Sum_of_even_sum.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main() {
+    //Saurav kumar 11755
+    int n, sum = 0;
+    printf("Enter a integer: ");
+    scanf("%d", &n);
 
+    for(int i=1;i<=n;i++) {
+        if(i%2!=0){
+        continue;
+    }
+        sum += i;
+}
+    printf("Even sum = %d", sum);
+    return 0;
+}
+//output 
+//Enter the value of n: 9
+//Sum of even numbers from 1 to 9 is: 20
