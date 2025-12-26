b8b8d19e8352f4c0c2468531badaa5b4912e225c
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:29:55 +0530

    Implement triangle printing function
    Added a function to print a right-angled triangle pattern based on user input.

--- Practical 8/Right angled tringle.c
+++ Practical 8/Right angled tringle.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+//Saurav kumar 11755
+void printTriangle(int n){
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }    
+}
+int main(){
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    printTriangle(n);
+    return 0;
+}
+//OUTPUT:
+//Enter a Number: 4
+//*
+//**
+//***
+//****
