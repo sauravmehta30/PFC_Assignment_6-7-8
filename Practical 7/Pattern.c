0fbf7da031cc61289be16b26274908ad564cc52d
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:28:31 +0530

    Implement pattern printing in Pattern.c
    Added a C program to print a pattern of letters.

--- Practical 7/Pattern.c
+++ Practical 7/Pattern.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main(){
+    //Saurav kumar 11755
+    int n=4;
+    for (int i=1;i<=n;i++){
+        char ch ='A';
+        for (int j=1;j<=i;j++) {
+            printf("%c", ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//OUTPUT:
+//A
+//AB
+//ABC
+//ABCD
