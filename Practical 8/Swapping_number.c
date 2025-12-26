82790ab76a47e1e323d09cf7320a1b229d198bf0
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:33:29 +0530

    Add swap function and main for number swapping
    Implement a swap function to exchange two integers.

--- Practical 8/Swapping_number.c
+++ Practical 8/Swapping_number.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+void swap(int*a,int*b){
+    //Saurav Kumar 11755
+    int temp;
+    temp =*a;
+    *a=*b;
+    *b=temp;
+}
+int main() {
+    int x, y;
+    printf("Enter two numbers:\n");
+    scanf("%d %d",&x,&y);
+    printf("Before swapping:x=%d,y=%d\n",x,y);
+    swap(&x, &y); 
+    printf("After swapping: x=%d, y=%d\n",x,y);
+    return 0;
+}
+//OUTPUT:
+//Enter two numbers:
+//6 8
+//Before swapping:x=6,y=8
+//After swapping: x=8, y=6
