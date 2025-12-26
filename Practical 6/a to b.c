d65beb7dcbbf5fb969a1df55228f9e9d6db7c366
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:18:52 +0530

    Implement number range printer in a to b.c
    Added a program to print numbers between two user-defined values.

--- Practical 6/a to b.c
+++ Practical 6/a to b.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main() {
+    //Saurav kumar 11755
+    int A,B;
+    printf("Enter the value of A: ");
+    scanf("%d", &A);
+    printf("Enter the value of B: ");
+    scanf("%d", &B);
+    printf("Numbers from %d to %d are:\n", A, B);
+    for (int i=A;i<=B;i++){
+        printf("%d ", i);
+    }
+    return 0;
+}
+//OUTPUT:
+//Enter the value of A: 2
+//Enter the value of B: 9
+//Numbers from 2 to 9 are:
+//  2 3 4 5 6 7 8 9 %
