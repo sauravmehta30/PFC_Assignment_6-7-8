ba78bd548ba045769670f0c6cc516a935da9266d
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:32:29 +0530

    Implement sumOfDigits function in Odd_sum.c
    Added a function to calculate the sum of digits of a number.

--- Practical 8/Odd_sum.c
+++ Practical 8/Odd_sum.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+//Saurav Kumar 11755
+int sumOfDigits(int n){
+    int sum = 0;
+    if(n<0){
+        n=-n;   
+    }
+    while(n>0){
+        sum +=n%10;
+        n/=10;
+    }
+    return sum;
+}
+int main() {
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    printf("%d", sumOfDigits(n)); 
+    return 0;
+}
+//OUTPUT:
+//Enter a Number: 678 
+// 21
