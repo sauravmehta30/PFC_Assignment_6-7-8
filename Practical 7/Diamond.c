84c36c9b6c6c1574b99b88fa78bb4d710d4642f6
sauravmehta30 <sauravmehta4848@gmail.com>
2025-12-26 12:25:28 +0530

    Implement diamond pattern printing in Diamond.c
    Added a program to print a diamond shape pattern based on user input.

--- Practical 7/Diamond.c
+++ Practical 7/Diamond.c
@@ -1,1 +1,35 @@
-
+#include <stdio.h>
+int main(){
+    //Saurav kumar 11755
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for (int i=1;i<=n;i++) {
+        for (int space=1;space<=n-i;space++) {
+            printf(" ");
+        }
+        for (int star=1;star<=2*i-1;star++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i=n-1;i>=1;i--) {
+        for (int space=1;space<=n-i;space++) {
+            printf(" ");
+        }
+        for (int star=1;star<=2*i-1;star++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+//OUTPUT:
+//Enter the value of n: 4
+//   *
+//  ***
+// *****
+//*******
+// *****
+//  ***
+//   *
